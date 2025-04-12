19d2541919cf305c7a55b108c51b2b2cb38006d5
Ibrahim Gaber <ibrahim.gaber.official@gmail.com>
2025-04-12 18:07:51 +0200

    First-implement Respond with body

--- src/Server.cs
+++ src/Server.cs
@@ -14,6 +14,8 @@ int bytesRead = client.Receive(buffer);
 
 // Convert the byte array to a string
 string request = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+
 var requestLines = request.Split("\r\n");
 var requestLineParts = requestLines[0].Split(" ", 3); // get the path from the request line
 var path = requestLineParts[1];
@@ -23,6 +25,16 @@ byte[] response;
 if(path == "/"){
     // return a 200 OK response
     response = System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+}
+else if(path.StartsWith("/echo")){
+    // get the message from the path
+    var message = path.Split("/")[2];
+    // return a 200 OK response with the message
+    response = System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n" +
+                                                  "Content-Type: text/plain\r\n" +
+                                                  $"Content-Length: {message.Length}\r\n" +
+                                                  "\r\n" + 
+                                                  $"{message}");
 }
 else {
     // return a 404 Not Found response
