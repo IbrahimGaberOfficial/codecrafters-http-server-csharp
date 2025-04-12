2408efc9c3864864e462ea22da176a966ac56004
Ibrahim Gaber <ibrahim.gaber.official@gmail.com>
2025-04-12 19:08:21 +0200

    convert the program from high instructions to add program calss and main method

--- src/Server.cs
+++ src/Server.cs
@@ -1,58 +1,73 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Reflection;
 
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-Socket client = server.AcceptSocket();
+class Program
+{
+    static void Main(string[] args)
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 4221);
+        server.Start();
+        Socket client = server.AcceptSocket();
 
-// Read the request from the client
-byte[] buffer = new byte[1024];
-int bytesRead = client.Receive(buffer);
+        // Read the request from the client
+        byte[] buffer = new byte[1024];
+        int bytesRead = client.Receive(buffer);
 
-// Convert the byte array to a string
-string request = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        // Convert the byte array to a string
+        string request = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
 
-var requestLines = request.Split("\r\n");
-var requestLineParts = requestLines[0].Split(" ", 3); // get the path from the request line
-var path = requestLineParts[1];
+        var requestLines = request.Split("\r\n");
+        var requestLineParts = requestLines[0].Split(" ", 3); // get the path from the request line
+        var path = requestLineParts[1];
 
 
 
-byte[] response;
+        byte[] response;
 
-if(path == "/"){
-    // return a 200 OK response
-    response = System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+        if (path == "/")
+        {
+            // return a 200 OK response
+            response = System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+        }
+        else if (path.StartsWith("/echo"))
+        {
+            // get the message from the path
+            var message = path.Split("/")[2];
+            // return a 200 OK response with the message
+            response = System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n" +
+                                                          "Content-Type: text/plain\r\n" +
+                                                          $"Content-Length: {message.Length}\r\n" +
+                                                          "\r\n" +
+                                                          $"{message}");
+        }
+        else if (path.StartsWith("/user-agent"))
+        {
+
+            // get the headers from the request
+            var headers = requestLines[3].Split(": ");
+            var userAgentValue = headers[1];
+
+            response = System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n" +
+                                                         "Content-Type: text/plain\r\n" +
+                                                         $"Content-Length: {userAgentValue.Length}\r\n" +
+                                                         "\r\n" +
+                                                         $"{userAgentValue}");
+        }
+        else
+        {
+            // return a 404 Not Found response
+            response = System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
+
+        }
+
+        client.Send(response);
+
+        client.Close();
+        server.Stop();
+    }
+
+
 }
-else if(path.StartsWith("/echo")){
-    // get the message from the path
-    var message = path.Split("/")[2];
-    // return a 200 OK response with the message
-    response = System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n" +
-                                                  "Content-Type: text/plain\r\n" +
-                                                  $"Content-Length: {message.Length}\r\n" +
-                                                  "\r\n" + 
-                                                  $"{message}");
-}
-else if(path.StartsWith("/user-agent")){
 
-    // get the headers from the request
-    var headers = requestLines[3].Split(": ");
-    var userAgentValue = headers[1];
 
-     response = System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n" +
-                                                  "Content-Type: text/plain\r\n" +
-                                                  $"Content-Length: {userAgentValue.Length}\r\n" +
-                                                  "\r\n" + 
-                                                  $"{userAgentValue}");
-}
-else {
-    // return a 404 Not Found response
-    response = System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
-    
-}
-
-client.Send(response);
-
-client.Close();
-server.Stop();
