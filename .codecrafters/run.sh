120d5c0458e34691c6cc0ba9ac08efc0f323f56b
Ibrahim Gaber <ibrahim.gaber.official@gmail.com>
2025-04-12 14:50:16 +0200

    test

--- .codecrafters/run.sh
+++ .codecrafters/run.sh
@@ -8,4 +8,13 @@
 
 set -e # Exit on failure
 
+# Install .NET 8 if not present
+if [ ! -d "/usr/share/dotnet/shared/Microsoft.NETCore.App/8.0.0" ]; then
+    echo "Installing .NET 8..."
+    wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+    chmod +x dotnet-install.sh
+    ./dotnet-install.sh --version 8.0.0
+    export PATH="$HOME/.dotnet:$PATH"
+fi
+
 exec /tmp/codecrafters-build-http-server-csharp/codecrafters-http-server "$@"
