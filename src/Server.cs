b8ee3247fac6e8908bdde8fcddc6714eec3eb79d
Ibrahim Gaber <ibrahim.gaber.official@gmail.com>
2025-04-25 05:16:10 +0200

    start in refactoring to split GET and POST

--- src/Server.cs
+++ src/Server.cs
@@ -50,79 +50,18 @@ class Program
                 string[] requestLineParts = requestLines[0].Split(' ');
                 if (requestLineParts.Length < 2) return;
 
-                string path = requestLineParts[1];
-                byte[] response;
+                string path = requestLineParts[1]; // remove after final edits
+                byte[] response; // remove after final edits
 
                 // get the method type
                 string method = requestLineParts[0];
 
+                
+
                 if (method.Equals("GET"))
                 {
-                    if (path == "/")
-                    {
-                        response = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
-                    }
-                    else if (path.StartsWith("/echo/"))
-                    {
-                        string message = path.Substring(6);
-                        response = Encoding.UTF8.GetBytes(
-                            "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: text/plain\r\n" +
-                            $"Content-Length: {message.Length}\r\n" +
-                            "\r\n" +
-                            $"{message}");
-                    }
-                    else if (path.StartsWith("/files"))
-                    {
-                        // get file name
-                        string fileName = path.Substring("/files/".Length);
-                        var argv = Environment.GetCommandLineArgs();
-                        var currentDirectory = argv[2];
-                        string filePath = System.IO.Path.Combine(currentDirectory, fileName);
-
-                        if (System.IO.File.Exists(filePath))
-                        {
-                            string fileContent = System.IO.File.ReadAllText(filePath);
-                            string fileSize = fileContent.Length.ToString();
-
-                            response = Encoding.UTF8.GetBytes(
-                                "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: application/octet-stream\r\n" +
-                                $"Content-Length: {fileSize}\r\n" +
-                                "\r\n" +
-                                $"{fileContent}");
-                        }
-                        else
-                        {
-                            response = Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
-                        }
-
-
-                    }
-                    else if (path == "/user-agent")
-                    {
-                        string userAgent = "";
-                        foreach (var line in requestLines)
-                        {
-                            if (line.StartsWith("User-Agent:"))
-                            {
-                                userAgent = line.Substring(11).Trim();
-                                break;
-                            }
-                        }
-                        response = Encoding.UTF8.GetBytes(
-                            "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: text/plain\r\n" +
-                            $"Content-Length: {userAgent.Length}\r\n" +
-                            "\r\n" +
-                            $"{userAgent}");
-                    }
-                    else
-                    {
-                        response = Encoding.UTF8.GetBytes("HTTP/1.1 404 Not Found\r\n\r\n");
-                    }
-
-                    await client.SendAsync(response, SocketFlags.None);
+                    GetRequestHandling.GetRequestHandler.HandleGetRequest(path);
+                   
                 }
                 else if (method.Equals("POST"))
                 {
